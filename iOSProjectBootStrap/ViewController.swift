d5a9565bf8e8eafcdaef738739a9c13392e86375
Sanooj Pollayil Sadananadan <sanooj.pollayilsadananadan@cognizant.com>
2020-06-27 22:27:09 +0100

    Initial Commit

---
+++ iOSProjectBootStrap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSProjectBootStrap
+//
+//  Created by carvak on 27/06/2020.
+//  Copyright © 2020 0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
